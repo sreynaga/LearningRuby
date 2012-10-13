a1ac933acd4db09c6b8a7b9fa5922722be7e7052
sreynaga <sergio.reynaga@gmail.com>
2012-10-13 14:28:09 -0700

    Ejercicio de Lambdas en Ruby

---
+++ Lambdas.rb
@@ -0,0 +1,19 @@
+class Lambdas
+	def initialize()
+		
+	end
+	def lambdas()
+		lamb = lambda do |nombre|
+			if nombre == 'Sergio'
+				return 'Hola Sergio'
+			else
+				return 'Hola Anonimo'
+			end
+		end
+		puts lamb.call('Sergio')
+	end
+end
+
+objeto = Lambdas.new()
+objeto.lambdas
+gets()
