7a0d7e61cf39961feda7b6facc38a1c377a3bc67
sreynaga <sergio.reynaga@gmail.com>
2012-10-13 15:08:50 -0700

    Utilizacion de Blocks en Ruby

---
+++ Blocks.rb
@@ -0,0 +1,20 @@
+# Block : Otra forma de agrupar codigo en Ruby
+
+class Array
+	def iterar
+		self.each_with_index do | n, i |
+			self[i] = yield(n)
+		end
+	end
+end
+
+arreglo = [1,2,3]
+arreglo.iterar do | n |
+	n**2
+end
+
+for i in arreglo
+	puts i
+end
+
+gets()
